01e8a90daad918bd7d4d8f055afffc16457570b9
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2022-05-23 21:43:12 +0430

    page1

---
+++ database/dbUtils.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+PATH = 'database\screws'
+def get_screw_path(name):
+    return os.path.join(PATH, name)
+
+
+
+def get_screws_list():
+    return os.listdir(PATH)
+
+
+def add_screw(name):
+    path = get_screw_path(name)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+        return True
+    return False
+
+    
+def remove_screw(name):
+    path = get_screw_path(name)
+    if os.path.isdir(path) and len(os.listdir(PATH))>0:
+        shutil.rmtree(path)
+        return True
+    return False
+
+
+
+
+if __name__ == '__main__':
+    print(get_screws_list())
---
+++ database/screwDB.py
@@ -0,0 +1,244 @@
+
+import json
+import os
+
+import cv2
+
+
+class screwJson():
+    
+    def __init__(self,):
+        self.data = {}
+
+
+    #-----------------------------------------
+    def read(self, path, dircetion):
+        path = os.path.join(path, dircetion + '.json')
+        with open(path) as jfile:
+            file = json.load(jfile)
+        
+        self.data = file
+        self.img = cv2.imread( self.data['img_path'] )
+        
+
+    def write(self,path):    
+        path = os.path.join(path, self.data['direction'] + '.json')
+        with open(str(path), 'w') as f:
+            json.dump(self.data, f)#,indent=4, sort_keys=True)
+            
+    #-----------------------------------------
+    def set_name(self,name):
+        self.data['name'] = name
+    
+    def get_name(self):
+        return self.data['name'] 
+    
+    #-----------------------------------------
+    def set_img_path(self,path):
+        self.img = cv2.imread(path)
+        self.data['img_path'] = path
+    
+    def get_img_path(self):
+        return self.data['img_path'] 
+    #-----------------------------------------
+    
+    def get_img(self):
+        return self.img
+    #-----------------------------------------
+    def set_direction(self, dir):
+        self.data['direction'] = dir
+    
+    def get_direction(self):
+        return self.data['direction'] 
+    
+    #-----------------------------------------
+    def set_main_thresh(self, thresh):
+        self.data['main_thresh'] = thresh
+        
+    def get_main_thresh(self):
+        return self.self.data['main_thresh']
+    
+    #-----------------------------------------
+    def set_main_roi(self,pt1,pt2):
+        self.data['main_roi'] = [ list(pt1) , list(pt2) ]
+    
+    def get_main_roi(self):
+        return self.data['main_roi']
+    
+    #-----------------------------------------
+    
+    
+    # def create_json_dataset(self,parms):
+        
+
+    #     self.set_name(parms['name']) 
+    #     self.set_img_path(parms['img_path']) 
+    #     #self.set_img(parms['img']) 
+    #     self.set_main_roi(parms['main_roi']) 
+
+
+
+    #     self.dataset_details['basic']=self.main_parms
+    #     self.dataset_details['classification']=self.classification_details
+    #     self.dataset_details['binary']=self.binary_details
+
+    #         # return {'user_name':user_name,'user_id':user_id,'dataset_name':dataset_name,'path':path,'max_size':max_size}
+    #     path=os.path.join(parms['path'],parms['dataset_name'])
+    #     self.write(os.path.join(path,str(parms['dataset_name']+'.json')))
+    #     try:
+    #         pathStructure.create_dataset_stracture(path)
+    #     except:
+    #         path
+    # def write(self,path):    
+    #     #print('write',path)
+    #     with open(str(path), 'w') as f:
+    #         json.dump(self.dataset_details, f,indent=4, sort_keys=True)
+    #         # json.dump(self.classification, f,indent=4, sort_keys=True)
+
+
+        
+    # def set_user_name_database(self,name):
+    #     self.user_name_database=name
+    # #--------------------------------------------------------
+    # #
+    # #--------------------------------------------------------
+    # def set_user(self, name):
+    #     self.main_parms['user_name'] = name
+
+    # def set_user_id(self, user_id):
+    #     self.main_parms['user_id'] = user_id
+
+
+    # def set_dataset_name(self, name):
+    #     self.main_parms['dataset_name'] = name
+
+    # def set_path(self, path,name):
+    #     self.main_parms['path'] = str(path+name)
+
+    # def set_max_size(self, max_size):
+    #     self.main_parms['max_size'] = max_size
+    
+    # def set_create_date(self,date):
+    #     self.main_parms['date_created'] = date_funcs.get_date()
+    
+    # def set_modify_date(self,date):
+    #     self.main_parms['date_modify'] = date
+    
+    # def set_count_defect(self,num):
+    #     self.binary_details['count_defect'] = num
+    
+    # def set_count_perfect(self,num):
+    #     self.binary_details['count_perfect'] = num
+
+    # def set_classification_parms(self,defects_list):
+
+    #     self.classification_details['defects_list'] = list(defects_list)
+
+    # def set_parms_classification(self,name,count):
+
+    #     self.classification_details[name] = count
+
+    # ######################################
+    # #  Modify        ///////////////
+
+    # def modify_defect(self,reset=False):
+    #     file=self.read_modify()
+    #     count=file['binary']['count_defect']
+    #     if reset:
+    #         count=-1
+    #     file['binary']['count_defect']=count+1
+    #     print('modify',file)
+    #     try:
+    #         file=self.modify_date(file)
+    #     except:
+    #         print('eror modify date')
+    #         pass
+    #     self.write_modify(file)
+    
+    # def modify_perfect(self,reset=False):
+    #     file=self.read_modify()
+    #     count=file['binary']['count_perfect']
+    #     if reset:
+    #         count=-1
+    #     file['binary']['count_perfect']=count+1
+    #     try:
+    #         file=self.modify_date(file)
+    #     except:
+    #         pass
+    #     self.write_modify(file)
+    
+    # def add_update_classification(self,name,AI=True,number=0):
+    #     count=number
+    #     file=self.read_modify()
+
+    #     if count !=0:
+    #         AI=False
+
+    #     if str(name) in file['classification'].keys():
+    #         if AI:
+    #             print('ai')
+    #             print((file['classification'][name]['count']))
+    #             count=int(file['classification'][name]['count'])
+    #             count_dict={'count':count+1}
+    #             file['classification'][name]=count_dict
+    #         else :
+    #             count_dict={'count':count}
+    #             file['classification'][name]=count_dict
+
+    #     else :
+    #         count_dict={'count':count}
+    #         name_dict={name:count_dict}
+    #         file['classification'].update(name_dict)
+    #         print(file)
+    #         # self.add_update_classification(name,AI,count)
+    #     self.write_modify(file)
+
+
+    # def read_modify(self):
+    #     print('user_name',self.user_name_database)
+    #     default_name = self.db.get_default_dataset(self.user_name_database)
+    #     print('default_name',default_name)
+    #     path = self.db.get_path_dataset(default_name)
+    #     path=os.path.join(path,default_name)
+    #     print('path',path)
+    #     with open(path+'.json') as jfile:
+    #         file = json.load(jfile)
+    #     return file
+
+
+    # def modify_date(self,file):
+    #     file['basic']['date_modify']=date_funcs.get_datetime()
+    #     return file
+
+
+    # def write_modify(self,file):
+    #     path = os.path.join(file['basic']['path'],file['basic']['dataset_name'])
+    #     with open(str(path+'.json'), 'w') as f:
+    #         json.dump(file, f,indent=4, sort_keys=True)
+
+
+    
+
+
+
+
+
+if __name__=='__main__':
+    import dbUtils
+    path = dbUtils.get_screw_path('pich2')
+    screw  = screwJson()
+    
+    #screw.set_name('aaaaaaa')
+    #screw.set_direction('ccccc')
+    #screw.write(path)
+    path = 'screws\\pich2\\top.json'
+    path = 'database\\screws\\Pich2\\top.json'
+    
+    a = screw.read(path)
+    # j = dataset_json()
+    # j.set_user_name_database('ali')
+    # i=1
+    # # j.add_update_classification('ads3',number=11)
+    # # for i in range(100):
+    # # j.modify_defect()
+    # j.modify_perfect()
