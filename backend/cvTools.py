6af6d3cfc68c5e8127162f1430291b9f865a8876
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2022-05-28 20:25:59 +0430

    functions for side cam added

--- backend/cvTools.py
+++ backend/cvTools.py
@@ -1,8 +1,9 @@
+from turtle import left
 import cv2
 from cv2 import circle
 from cv2 import contourArea
 import numpy as np
-
+import pandas as pd
 THRESH_C = 7
 
 
@@ -198,9 +199,14 @@ def correct_rotation_angle(mask , left_or_right='left'):
     
     box = cv2.minAreaRect(screw_body)
     _,_,angle = box
-    # rect_cnt = cv2.boxPoints(box)
-    # rect_cnt = np.int0( rect_cnt )
-    return angle - 90
+
+    points = cv2.boxPoints(box)
+    points = np.int0(points)
+    if angle > 45:
+        angle = angle - 90
+    else:
+        angle = angle - 0
+    return angle, points
     
     
 
@@ -215,10 +221,171 @@ def side_screw_bounding_rect(mask):
     x2,y2 = points.max(axis=0)[0]
     
     return (x1,y1) , (x2,y2)
+
+
+
+def find_screw_thread(mask , rect_roi,  min_diff = 5, max_bad_iter = 5 ):
+    ys, xs = np.nonzero( crop_rect( mask, rect_roi ) )
+    pts = np.vstack((xs,ys)).transpose()
+    up_edge_pts = np.array ( pd.DataFrame(pts).groupby(0).min().reset_index().values.tolist()) #return point with min y and same x
     
+    rezve_pts_h = []
+    rezve_pts_l = []
+    
+    max_pt = up_edge_pts[0]
+    min_pt = up_edge_pts[0]
+    iter = 0
+    find_max = True
+    for i in range(1,len(up_edge_pts)):
+        
+        #upper y is lower in value
+        if find_max:
+            if up_edge_pts[i][1] <= max_pt[1]:
+                max_pt = up_edge_pts[i]
+                iter = 0
+            else:
+                iter+=1
+                
+            if iter> max_bad_iter:
+                iter = 0
+                find_max = False
+                min_pt = [max_pt[0] , max_pt[1]]
+                
+        
+        else:
+            if up_edge_pts[i][1] >= min_pt[1]:
+                min_pt = up_edge_pts[i]
+                iter = 0
+            
+            else:
+                iter+=1
+            
+            if iter > max_bad_iter:
+                iter = 0
+                find_max = True
+                if abs(max_pt[1] - min_pt[1]) >= min_diff:
+                    rezve_pts_h.append( [max_pt[0], max_pt[1]] )
+                    rezve_pts_l.append( [min_pt[0], min_pt[1]] )
+                    max_pt = [min_pt[0] , min_pt[1] ]
+    
+    rezve_pts_l, rezve_pts_h = np.array(rezve_pts_l), np.array(rezve_pts_h)          
+    
+    start_point = np.array(rect_roi[0])
+    rezve_pts_l = rezve_pts_l + start_point
+    rezve_pts_h = rezve_pts_h + start_point
+    return rezve_pts_l, rezve_pts_h
 
 
+
+
+def find_horizental_edges(mask , rect_roi ):
+    ys, xs = np.nonzero( crop_rect( mask, rect_roi ) )
+    pts = np.vstack((xs,ys)).transpose()
+    
+    right_pts = np.array ( pd.DataFrame(pts).groupby(1).max().reset_index().values.tolist()) #return point with max x and same y
+    right_pts = np.array([right_pts[:,1],right_pts[:,0]]).transpose()
+
+    left_pts = np.array ( pd.DataFrame(pts).groupby(1).min().reset_index().values.tolist()) #return point with max x and same y
+    left_pts = np.array([left_pts[:,1],left_pts[:,0]]).transpose()
+    
+    start_point = np.array(rect_roi[0])
+    
+    right_pts = right_pts + start_point
+    left_pts = left_pts + start_point
+    return left_pts, right_pts
+
+
+
+def find_vertical_edges(mask, rect_roi):
+    ys, xs = np.nonzero( crop_rect( mask, rect_roi ) )
+    pts = np.vstack((xs,ys)).transpose()
+    up_edge_pts = np.array ( pd.DataFrame(pts).groupby(0).min().reset_index().values.tolist()) #return point with min y and same x
+    down_edge_pts = np.array ( pd.DataFrame(pts).groupby(0).max().reset_index().values.tolist()) #return point with min y and same x
+    
+    
+    start_point = np.array(rect_roi[0])
+    up_edge_pts = up_edge_pts + start_point
+    down_edge_pts = down_edge_pts + start_point
+    return up_edge_pts, down_edge_pts
+
+
+def draw_vertical_point(img, pts, color, thicknes=4):
+    for i in range(-thicknes//2, thicknes//2):
+        img[ pts[:,1], pts[:,0] + i] = color
+    return img
+
+def draw_horizental_point(img, pts, color, thicknes=4):
+    for i in range(-thicknes//2, thicknes//2):
+        img[ pts[:,1] + i , pts[:,0]] = color
+    return img
+
+
+def draw_points(img, pts, color, radius=4):
+    for pt in pts:
+        cv2.circle(img, tuple(pt), radius, color, thickness=-1)
+    return img
+    
 if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    
+    
+    img = cv2.imread('sample images/New folder/31x_1_3.png')
+    print('________________________________________________________________________')
+    
+    rect =[ [20, 60] , [1360, 480] ]
+    thresh = 50
+    
+    mask_roi = rects2mask( img.shape[:2] , [rect] )    
+    thresh_mask = threshould(img, thresh, mask_roi=mask_roi, inv=True)
+    cv2.imshow( 'thresh_mask_befor_rateton', thresh_mask)
+    
+    
+    #--------------------------------------------------------
+    mask_unbelt, belt_pos = remove_belt( thresh_mask )
+    angle, box = correct_rotation_angle(mask_unbelt)
+    thresh_mask = rotate_image(thresh_mask,  angle   )
+    img = rotate_image(img,  angle   )
+    
+    
+    #--------------------------------------------------------
+    total_lenght_roi = [ [20, 220] , [1360, 290] ]
+    left_pts, right_pts = find_horizental_edges(thresh_mask, total_lenght_roi)
+    
+    img[ left_pts[:,1] , left_pts[:,0] ] = [0,0,255]
+    img[ right_pts[:,1] , right_pts[:,0] ] = [0,0,255]
+
+    img = draw_vertical_point(img, left_pts , (0,0,255), 2 )
+    img = draw_vertical_point(img, right_pts , (0,0,255), 2 )
+    #--------------------------------------------------------
+    radiuse_body_roi = [ [660, 130] , [910, 380] ]
+    
+    up_pts, down_pts = find_vertical_edges(thresh_mask, radiuse_body_roi)
+    
+    img = draw_horizental_point(img, up_pts , (0,255,0), 2 )
+    img = draw_horizental_point(img, down_pts , (0,255,0), 2 )
+    
+    #--------------------------------------------------------
+    rezve_rect_roi = [ [100, 60] , [550, 400] ]
+    male_thread_l, male_thread_h = find_screw_thread( thresh_mask, rezve_rect_roi,  min_diff=5)
+    
+    img = draw_points(img, male_thread_h, (230,200,150), 3)
+    img = draw_points(img, male_thread_l, (0,50,200), 3)
+    #--------------------------------------------------------
+    
+    
+    
+    
+        
+        
+    
+    
+    
+    cv2.imshow( 'thresh_mask', thresh_mask)
+    cv2.imshow( 'img', img)
+    cv2.waitKey(0)
+    pass
+    
+    #------------
     
     # mask_img = cv2.imread('mask.jpg', 0)
     # img = cv2.imread( 'img.jpg' , 0 )
@@ -243,29 +410,29 @@ if __name__ == '__main__':
     
     
     
-    img = cv2.imread('sample images/test1.jpg')
+    # img = cv2.imread('sample images/test1.jpg')
     
-    mask = threshould(img, 50, None, True)
-    mask_unbelt, belt_pos = remove_belt( mask )
+    # mask = threshould(img, 50, None, True)
+    # mask_unbelt, belt_pos = remove_belt( mask )
     
-    angle = correct_rotation_angle(mask_unbelt)
+    # angle = correct_rotation_angle(mask_unbelt)
     
-    img = rotate_image(img,  angle  )
-    rotated_mask = rotate_image(mask_unbelt,  angle )
-    
-    
-    
-    pt1,pt2 = side_screw_bounding_rect(rotated_mask)
-    
-    img = cv2.rectangle( img , pt1, pt2, (255,0,0))
-    #img = cv2.drawContours(img, [rect_cnt], 0, (0,0,255), thickness=5)
+    # img = rotate_image(img,  angle  )
+    # rotated_mask = rotate_image(mask_unbelt,  angle )
     
     
     
-    cv2.imshow('mask1', mask)
-    cv2.imshow('res_mask', mask_unbelt)
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
-    pass
+    # pt1,pt2 = side_screw_bounding_rect(rotated_mask)
+    
+    # img = cv2.rectangle( img , pt1, pt2, (255,0,0))
+    # #img = cv2.drawContours(img, [rect_cnt], 0, (0,0,255), thickness=5)
+    
+    
+    
+    # cv2.imshow('mask1', mask)
+    # cv2.imshow('res_mask', mask_unbelt)
+    # cv2.imshow('img', img)
+    # cv2.waitKey(0)
+    # pass
     
     
